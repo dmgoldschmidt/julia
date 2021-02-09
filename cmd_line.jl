190861cf20e9b5ac14fcb08f21c67ef3000c13f5
david <david@goldbell.org>
2021-02-08 16:52:00 -0800

    Added a lot of comments and changed the function name to parse. Changed name of get_arg to get_val and changed return to nothing if requestion option did not appear.

--- cmd_line.jl
+++ cmd_line.jl
@@ -1,4 +1,44 @@
 #! /home/david/julia-1.5.3/bin/julia
+"""
+command line(s) parses s = Array{String} whose entries are strings that we call fields.
+The default argument is ARGS, which gives the command line by which the enclosing julia 
+program was invoked.  
+
+Fields beginning with the character '-' are called "options", all other fields are called 
+"values" with two exceptions:
+  1. negative numbers
+  2. the one-character substring "-"
+which are also values. 
+
+The second character of an option may also be '-', in which case the remainder of the field is 
+the option name which cannot include the character '='. A field of the form "--option=value" is equivalent
+to the pair of consecutive fields "--option",  "value".  
+
+If the second field character is a letter, that character becomes the option
+name and the remainder of the field, if it is non-empty, becomes the value 
+associated to that option. 
+ 
+command_line returns an instance of the struct cmd_line below, consisting of two Array{String}
+arrays of the same length.  This creates a pairing (option[i],value[i])
+meaning that option[i] has immediately preceded value[i] in the input s.
+However, if two options (resp. values) occur in sequence, the first option
+(resp. second value) is given the value "".  This allows maximum flexibility
+for the input, as the input array can have options and values in any order as long as the value associated
+with an option (if any) follows the option immediately.  The caller doesn't need to know anything about
+the order of the inputs and just receives a pair of String Arrays.  Or the caller can use the convenience
+function "get_val(option)" which returns the value associated to option, or "" if there is none, or it
+returns the nothing value if the requested option was not present.
+
+However, there is an ambiguous parse, namely when, e.g., the
+parser encounters "-f file" it means that the option "f" has the value "file",
+alothough it might mean (but doesn't) that the option "f" has no associated value and the
+value "file" has no associated option because both can legally appear as singletons. 
+It is up to the user to disambiguate these two cases.
+
+"""
+
+ 
+
 struct cmd_line
   option::Array{String}
   value::Array{String}
@@ -14,7 +54,7 @@ function isnumber(s)
 end
 
 
-function command_line(s::Array{String} = ARGS)
+function parse(s::Array{String} = ARGS)
   c = cmd_line([],[])
   last_was_option::Bool = false
   item_no = 0
@@ -24,7 +64,7 @@ function command_line(s::Array{String} = ARGS)
 #    println("item no $item_no: ")
     if item[1] == '-'
       if length(item) == 1 || isnumber(item)
-        #it's the value '-'  or a negative number
+        # special case: it's the value '-'  or a negative number
         if !last_was_option #two values in a row
           push!(c.option,"")
         end
@@ -32,7 +72,7 @@ function command_line(s::Array{String} = ARGS)
         last_was_option = false
         continue
       end
-      # OK, it's an option
+      # general case:  it's an option
       if last_was_option # more than one option in a row
 #        println("two options in a row at item no ",item_no," = ",item)
         push!(c.value,"") # previous option has no corresponding value
@@ -41,7 +81,7 @@ function command_line(s::Array{String} = ARGS)
       if item[2] == '-' # long style --option
         x = findall("=",item) # NOTE: item[3:end] returns "" if length == 2 
         if(length(x) > 0) # it's of the form --option=value
-          i = x[1][1]
+          i = x[1][1] # item[i] = '='
 #          println("i = $i")
           push!(c.option,item[3:i-1])
           push!(c.value,item[i+1:end])
@@ -50,9 +90,9 @@ function command_line(s::Array{String} = ARGS)
           push!(c.option,item[3:end])
           last_was_option = true
         end
-      else # single letter option followed optionally by value
+      else # single letter option 
         push!(c.option,string(item[2]))
-        if length(item) > 2
+        if length(item) > 2 # value follows immediately with no intervening space
           push!(c.value,item[3:end])
           last_was_option = false
         else
@@ -71,24 +111,21 @@ function command_line(s::Array{String} = ARGS)
       push!(c.value,item)
     end
   end
-  if last_was_option
+  if last_was_option # last item was an option with no value
     push!(c.value,"")
   end
   return c
 end
 
-function get_arg(s::String)
-  c = command_line()
+function get_val(option::String, s::Array{String} = ARGS) # get the value of option from the command line
+  c = parse(s)
   i = 0
-  r = ""
+  r = nothing
   for opt in c.option
     i += 1
-    if opt == s
-      if length(opt) == 1 && c.value[i] == "" && length(c.value) >= i+1 && c.option[i+1] == ""
-        r = c.value[i+1] # this is the ambiguous case. If it looks like e.g. "-f value" return value
-      else
-        r = c.value[i]
-      end
+    if opt == option
+      r = c.value[i]
+      break; #NOTE: if there is a duplicated option, the first one is chosen
     end
   end
   return r 
@@ -96,14 +133,14 @@ end
 
     
 
-c = command_line()
+c = parse()
 println("options: ",c.option)
 println("values: ",c.value)
 println("\n")
 r = ""
-r = get_arg(ARGS[1])
-if r != ""
-  println("found ",ARGS[1], ", arg = ",r)
+r = get_val(ARGS[1])
+if r != nothing
+  println("found $(ARGS[1]) = $r")
 else
   println(ARGS[1]," not found")
 end
