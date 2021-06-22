21e532423cc38728afcc07ff49d2da56808bf164
dmgoldschmidt <dmgoldschmidt@gmail.com>
2021-06-22 00:09:17 +0000

    deleted test_fts_flowsets after moving it to fts_flowsets.jlh fts_flowsets.jlh: began mods to get rid of functors, pass params in calling sequences, reference channels by worker no. modified get_fts_flowsets: added main, command line options, began to make mods to look like the c++ version (but with histograms)

--- get_fts_flowsets.jl
+++ get_fts_flowsets.jl
@@ -1,220 +1,148 @@
-include("util.jl")
-include("stats.jl")
-
-@enum Ident OPEN=1 DATA=2 ERROR=3 EOF=4 QUIT=5 RAW=6 
-
-mutable struct Signal
-  s::Threads.Atomic{Bool}
-  function Signal(b::Bool)
-    return new(Threads.Atomic{Bool}(b))
-  end
-#  function Signal(b::Threads.Atomic{Bool})
-#    return new(b)
+#!/home/dmgoldschmidt/julia-1.6.1/bin/julia
+import GZip
+if !@isdefined(CommandLine_loaded)
+  include("CommandLine.jl")
 end
-function (sig::Signal)()
-  t = Threads.Atomic{Bool}(true)
-  return sig.s[] == t[] ? true : false
+if !@isdefined(sort_loaded)
+  include("sort.jl")
+end
+if !@isdefined(util_loaded)
+  include("util.jl")
 end
 
-mutable struct NetflowData #reader -> master
-  time::Int64 # rounded to nearest second
-  netident::String # enip|webip|webport
-  field::Array{Float64}
-  dir::Int8
-  function NetflowData(t::Int64 = 0,n::String = "",b::Matrix{Float64} = zeros(8,16),d = 0)
-    return new(t,n,b,d)
-  end
-end
+include("fts_flowsets.jlh")
+using Printf
 
-mutable struct WorkerMessage
-  ident::Ident
-  record::NetflowData
-end
-
-mutable struct OutputData #worker -> writer
-  netident::String
-  data::Array{Float64}
-  nrecs::Int64
-end
-
-
-mutable struct ReaderMessage
-  ident::Ident
-  payload::String
-end
-
-mutable struct ParserMessage
-  ident::Ident
-  line::String
-  data::NetflowData
-end
-
-mutable struct WriterMessage
-  ident::Ident
-  output::OutputData
-end
-
-struct Reader
-  chan::Channel
-  file::String
-  chan_no::Int64
-end
-
-function (r::Reader)()
-  t = Threads.threadid()
-  #println("opening file $(r.file) on thread $t")
-  stream = tryopen(r.file)
-  #println("$(r.file) is open")
-  for line in eachline(stream)
-    #println("thread $t: sending $line")
-    put!(r.chan,ReaderMessage(DATA,line))
-    #println("thread $t: DATA sent")
-  end
-  close(stream)
-  #    #println("thread $t: closed $file. Sending eof")
-  put!(r.chan,ReaderMessage(EOF,""))
-  #    #println("thread $t: EOF sent")
-end
-
-struct Writer
-  chan::Channel
-  file::String
-  chan_no::Int64
-end
-
-function (writer::Writer)()
-  stream = tryopen(writer.file,"w")
-  t = Threads.threadid()
-  while true
-    #println("writer on thread $t: waiting for OutputData")
-    msg = take!(writer.chan)
-    #println("thread $t: got $(msg.ident) $(msg.output.netident)")
-    if msg.ident == QUIT
-      #println("thread $t: got QUIT")
-      close(stream)
-      #println("thread $t: closed $(writer.file)")
-      msg.ident = EOF
-      put!(writer.chan,msg)
-      break
-    else
-      #println("thread $t: writing $(msg.output.netident)")
-      line = "$(msg.output.netident)"
-      for x in msg.output.data; line = line*(" $x"); end
-      #println(stream,line)
-    end
+function writeit()
+  i = 0
+  dummy = [1.0,.123456789]
+  nlines = [0,0]
+  max = [2,3]
+  ntries = 0
+  msg = 0
+  while nlines[1] < max[1] || nlines[2] < max[2]
+    if nlines[i+1] < max[i+1]
+      output = OutputData("line$(nlines[i+1]+1) for $(file[i+1]) on channel $(i+1)",dummy,2)
+      msg = WriterMessage(DATA,output)
+      println("master: sending $(msg.output.netident) to  channel $(i+1)")
+      put!(wchan[i+1],msg)
+      println("sent $(msg.output.netident) to channel $(i+1)")
+      nlines[i+1] = nlines[i+1]+1
+    end #if
+    i = (i+1)%2
   end #while
-  #println("writer exiting")
-end
-
-
-
-mutable struct My_Worker
-  my_worker_no::Int64
-  master::Channel
-  writer::Channel
-  duration::Int64
-  min_recs::Int64
-  active::Signal
-  ack::Signal
-  buckets::Matrix{Float64} # bucket boundaries
-#  ref::Matrix{Float64} # expected frac of total
-end
-
-mutable struct FlowsetData
-  start_time::Int64 
-  netident::String
-  nrecs::Int64
-  hist::Matrix{Int64}
-  io_pattern::Int64
-  function FlowsetData(t,i, h = ones(Int64,8,16), io = 0) #constructor
-    #println("constructing FlowsetData($time,$ident)")
-    return new(t,i,h,io) #basic flattening for the counts
+  output = OutputData("",dummy,2)
+  qmsg = [WriterMessage(QUIT,output) for i in 1:2]
+  for i in 1:2
+    println("master: sending QUIT on channel $i")
+    put!(wchan[i],qmsg[i])
+    println("master: QUIT sent on channel $i")
   end
-end
-start = (1,4)
-stop = (3,7)
-function(w::My_Worker)()
-  t = Threads.threadid()
-  #println("worker $(w.worker_no) on thread $t")
-  w.active = Signal(false)
-  #println("worker $(w.worker_no): set active signal to false")
-  current_data = 0#FlowsetData(0,"worker $(w.worker_no)",ones(Int64,8,16),0) # initialize current values
-  #println("worker $(w.worker_no): entering while loop")
-  while true
-    #println("worker $(w.worker_no) on thread $t: waiting for message")
-    msg = take!(w.master)
-    
-    if msg.ident == OPEN # open a new flowset
-      #println("worker $(w.worker_no) on thread $t: got $(msg.ident). active = $(w.active())")
-      if w.active() # an active worker can't be OPENed.  Must expire or get QUIT first
-        #println("worker $w.worker_no: got OPEN message while active.  Bailing out")
-        exit(0)
-      else
-        time = msg.record.time
-        netident = msg.record.netident
-        #println("initializing time = $time, netident = $netident")
-        current_data = FlowsetData(time,netident) # inactive, so re-initialize
-        w.active = Signal(true)
-      end
-      continue #while true
+  for i in 1:2
+    println("master: waiting for EOF on channel $i")
+    msg = take!(wchan[i])
+    if msg.ident != EOF
+      println(stderr,"comms error in writeit on channel $i.  Bailing out")
+      exit(1)
     end
-    
-    if msg.ident == DATA || msg.ident == QUIT
-      if !w.active(); continue; end  #ignore 
+    println("master: got EOF on channel $i") 
+  end
+  println("wrote $nlines")
+end
 
-    elseif msg.ident == QUIT || (msg.ident == DATA && msg.record.time - current_data.start_time > current_data.duration)   # flowset has expired
-      if current_data.nrecs >= w.min_recs #OK to write (if not, just ignore)
-        line = netident
-        chi_sq = Array{Float64}(undef,8)
-        for i in 1:8 #convert counts to tail probs for each feature
-          bucket_sum = 1 #flattening
-          for j in 1:16; bucket_sum += hist[i,j]; end
-          for j in 1:16
-            expected = bucket_sum/16.0 #= reference distro is uniform. The bucket sizes are non-uniform in general. They
-                                         are computed by a separate function making the overall distribution uniform, and 
-                                         they are set by the master task when the workers are launched. NOTE: this doesn't
-                                         work for iopattern! =#   
-            x = hist[i,j] - expected
-            chi_sq[i] = stats.gammaq(15,(x*x)/expected)
-          end #for j
-          line = line*" $chi_sq[i]" # add to the output line
-        end #for i
-        line = line*"\n"
-        output = OutputData(current_data.netident,line,nrecs)
-        put!(w.writer,WriterMessage(DATA,output)) # send it to the writer
-      end #output written
-      w.active = Signal(false)
-      if msg.ident == QUIT
-        w.ack[] = Signal(true) # acknowledge quit
-        break
-      end # my_worker will exit
+function readit()
+  i = done = 0
+  while done != 2
+    if isready(rchan[i+1])
+      println("master: waiting for DATA")
+      msg = take!(rchan[i+1])
+      if msg.ident == EOF
+        done += 1
+        println("master: EOF received.  done = $done")
+      else
+        println("master: $(msg.payload)")
+      end #if EOF
+    else
+#      println("channel $(i+1) not ready")
+      i = (i+1)%2 
+    end #if isready
+  end #while
+end
 
-    else # time has not expired.  Update current_data.hist from NetflowData
-      dir = msg.record.dir # direction of flow: 1 = input, 2 = output
-      for i in start[dir]:stop[dir] # input fields = 1,2,3.  output fields = 4,5,6,7
-        j = 0
-        while msg.record.field[i] > w.buckets[j] && j < 16 # find the bucket
-          j += 1
-        end
-        current_data.hist[i,j] += 1 #update the bucket count
-      end
-      current_data.iopattern = ((current_data.iopattern << 1) | (dir-1)) & 15 #update io pattern by one bit (dir-1) after left shift 1 
-      current_data.hist[8,current_data.iopattern] += 1 
-      current_data.nrecs += 1
-    end # msg.ident == DATA
-  end #while true
-end #function
-  
-    
+function main(cmd_line = ARGS)    
+  defaults = Dict{String,Any}(
+    "max_recs" => 0,
+    "netflow"=>"netflow.gz",
+    "file_dir" => "",
+    "out_file"=>"flowsets.txt",
+    "dim" => 8,
+    "reg_output" => false
+  )
+  cl = get_vals(defaults,cmd_line) # replace defaults with command line values if they are specified
+  println("parameters: $defaults")
 
-    
-    
-      
-        
-        
+  buckets = Matrix{Float64}(undef,8,15) #set up some buckets
+  for i in 1:8
+    for j in 1:15
+      buckets[i,j] = 1.0*j
+    end
+  end 
+  nworkers = 2
+  println("buckets = $(buckets[1,:])")
+  wchan = Channel(0) # one writer channel
+  stream = open(out_file,"w")
+  Threads.@spawn writer(stream) # launch the writer
+  println("writer is launched")
+  signal = Vector{Int64}(0,2)
+  println("signal: $signal")
+  Worker_chan = [Channel{WorkerMessage}(100) for i in 1:2] # set up two workers
 
+  worker_data = [WorkerData(i,300,10,buckets) for i in 1:2]
+  for i in 1:2
+    println("launching worker $i")
+    Threads.@spawn Worker(worker_data[i])
+  end
+  println("workers are launched")
+  sleep(5)
 
+  netident = ("one", "two")
+  for i in 1:2
+    println("sending OPEN to worker $i")
+    dummy = NetflowData(0,netident[i])
+    msg = WorkerMessage(OPEN,dummy)
+    println("opening Worker $i")
+    put!(Worker_chan[i],msg)
+    println("waiting for worker $i active signal")
+    while !my_worker[i].active(); end #wait for signal
+    println("worker $i is active")
+  end
 
+  for t in 1:20
+    worker_no = t%2
+    netflow = NetflowData(t,netident[worker_no])
+    netflow.dir = t < 10 ? 1:2
+    netflow.field = rand(1:16,8)
+    msg = WorkerMessage(DATA,netflow)
+    put!(Wchan[worker_no],msg)
+  end
 
+  for i in 1:2
+    put!(Wchan[i],WorkerMessage(QUIT,NetflowData()))
+    while !my_worker[i].ack(); end 
+    println("worker $i has acknowledged QUIT")
+  end
 
+end #main   
 
+# execution begins here
+          
+if occursin("cluster_flowsets.jl",PROGRAM_FILE)
+  #was cluster_flowsets.jl called by a command?
+  main(ARGS) 
+else
+  if isinteractive()
+    print("enter command line: ")
+    cmd = readline()
+    main(map(string,split(cmd)))
+  end
+end 
